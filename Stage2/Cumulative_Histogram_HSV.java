7cf2e4b3505931a8ab073a0a2232b0f4cad51f0b
anahitamirkhanyan <anahit_amirkhanyan@edu.aua.am>
2019-12-16 03:59:38 +0400

    it's 4am WHY IS SUMMARY REQUIRED

--- Stage2/Cumulative_Histogram_HSV.java
+++ Stage2/Cumulative_Histogram_HSV.java
@@ -18,9 +18,9 @@ public class Cumulative_Histogram_HSV implements PlugInFilter {
 
         // three arrays for H, S, and V channels
 
-        double[] hist_h = new double[256];
-        double[] hist_s = new double[256];
-        double[] hist_v = new double[256];
+        double[] hist_h = new double[360];
+        double[] hist_s = new double[100];
+        double[] hist_v = new double[100];
 
         
         double r, g , b, h, s, v, cmax, cmin, diff;
@@ -63,10 +63,6 @@ public class Cumulative_Histogram_HSV implements PlugInFilter {
 
                 h_i = (int)Math.round(h); s_i = (int)Math.round(s); v_i = (int)Math.round(v);
 
-                if(h_i > 255) h_i = 255;
-                if(s_i > 255) s_i = 255;
-                if(v_i > 255) v_i = 255;
-
                 hist_h[h_i]++;
                 hist_s[s_i]++;
                 hist_v[v_i]++;
@@ -74,46 +70,55 @@ public class Cumulative_Histogram_HSV implements PlugInFilter {
             }
         }
 
-        double [] cumulative_h = new double[256];
-        double [] cumulative_s = new double[256];
-        double [] cumulative_v = new double[256];
+        double [] cumulative_h = new double[360];
+        double [] cumulative_s = new double[100];
+        double [] cumulative_v = new double[100];
 
-        for(int i = 0; i < 256; i++){
-            if(i == 0){
+        for(int i = 0; i < 360; i++){
+            if(i == 0) 
                 cumulative_h[i] = hist_h[i];
+            else
+                cumulative_h[i] = cumulative_h[i-1] + hist_h[i];
+        }
+
+        
+        for(int i = 0; i < 100; i++){
+            if(i == 0){
                 cumulative_s[i] = hist_s[i];
                 cumulative_v[i] = hist_v[i];
             }
             else{
-                cumulative_h[i] = cumulative_h[i-1] + hist_h[i];
                 cumulative_s[i] = cumulative_s[i-1] + hist_s[i];
                 cumulative_v[i] = cumulative_v[i-1] + hist_v[i];
             }
         }
 
-        double[] norm_cum_h = new double[256];
-        double[] norm_cum_s = new double[256];
-        double[] norm_cum_v = new double[256];
+        double[] norm_cum_h = new double[360];
+        double[] norm_cum_s = new double[100];
+        double[] norm_cum_v = new double[100];
 
-        for(int i = 0; i < 256; i++){
+        for(int i = 0; i < 360; i++){
             norm_cum_h[i] = cumulative_h[i]/(ip.getWidth()*ip.getHeight());
+        }
+
+        for(int i = 0; i < 100; i++){
             norm_cum_s[i] = cumulative_s[i]/(ip.getWidth()*ip.getHeight());
             norm_cum_v[i] = cumulative_v[i]/(ip.getWidth()*ip.getHeight());
         }
 
 
         IJ.log("H");
-        for(int i = 0; i < 256; i++){
+        for(int i = 0; i < 360; i++){
             IJ.log(String.valueOf(norm_cum_h[i]));
         }
 
         IJ.log("S");
-        for(int i = 0; i < 256; i++){
+        for(int i = 0; i < 100; i++){
             IJ.log(String.valueOf(norm_cum_s[i]));
         }
 
         IJ.log("V");
-        for(int i = 0; i < 256; i++){
+        for(int i = 0; i < 100; i++){
             IJ.log(String.valueOf(norm_cum_v[i]));
         }
 	}
